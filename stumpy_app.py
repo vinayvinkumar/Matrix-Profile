8596c786811ffdbb4b6528bd0fcedc025a02f1b2
Vinay Kumar Ranganath Babu <vinayvinkumar@gmail.com>
2023-05-05 14:39:56 +0530

    minor changes

--- stumpy_app.py
+++ stumpy_app.py
@@ -25,16 +25,17 @@ def app():
     # Check if a file was uploaded
     if uploaded_file is not None:
         # Load the data into a Pandas dataframe
+        # data = pd.read_csv(uploaded_file) if uploaded_file.type == "application/vnd.ms-excel" else pd.read_excel(uploaded_file)
         data = pd.read_csv(uploaded_file) if uploaded_file.type == "text/csv" else pd.read_excel(uploaded_file)
 
         # Let the user select the column for time-series analysis
         column = st.selectbox("Select a column for time-series analysis", data.columns)
 
-        st.write("Time Series Data plot")
+        st.write("Visualizing the Time Series Data")
         
-        # Plot the time series graph
+        # Plot the data graph
         st.line_chart(data[column], use_container_width=True)
-        
+
         # Let the user choose the value of m for matrix profile analysis
         m = st.slider("Choose the value of m for matrix profile analysis", min_value=2, max_value=int(len(data[column]) / 2), value=30)
 
@@ -47,10 +48,26 @@ def app():
         # Compute the matrix profile using Stumpy's stump function
         matrix_profile = stumpy.stump(data[column], m)
 
-        st.write("Plot the matrix profile graph")
-        
-        # Plot the matrix profile graph
-        st.line_chart(matrix_profile[:, 0], use_container_width=True)
+        motif_idx = np.argsort(matrix_profile[:, 0])[0]
+        nearest_neighbor_idx = matrix_profile[motif_idx, 1]
+
+        st.write("Visualizing the Matrix Profile")
+
+        fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0}, figsize=(20,16))
+        plt.suptitle('Motif (Pattern) Discovery', fontsize='30')
+
+        axs[0].plot(data[column].values)
+        axs[0].set_ylabel(column, fontsize='20')
+        rect = Rectangle((motif_idx, 0), m, 40, facecolor='lightgrey')
+        axs[0].add_patch(rect)
+        rect = Rectangle((nearest_neighbor_idx, 0), m, 40, facecolor='lightgrey')
+        axs[0].add_patch(rect)
+        axs[1].set_xlabel('Time', fontsize ='20')
+        axs[1].set_ylabel('Matrix Profile', fontsize='20')
+        axs[1].axvline(x=motif_idx, linestyle="dashed")
+        axs[1].axvline(x=nearest_neighbor_idx, linestyle="dashed")
+        axs[1].plot(matrix_profile[:, 0])
+        st.pyplot(fig)
 
         # Compute the univariate motif using Stumpy's motifs function
         motifs = stumpy.motifs(data[column].values,matrix_profile[:, 0],max_matches=max_matches, max_motifs=max_motifs)
@@ -72,16 +89,5 @@ def app():
             st.line_chart(conc_motifs,use_container_width=True)
         else:
              st.write("No motifs found.")
-
-        # Plot the top 10 motifs
-        # if len(top_motifs) > 0:
-        #     st.write(f"Top 10 motifs:")
-        #     for i, motif in enumerate(top_motifs):
-        #         fig, ax = plt.subplots()
-        #         ax.plot(motif["indices"], motif["values"])
-        #         ax.set_title(f"Motif {i+1}")
-        #         st.pyplot(fig)
-        # else:
-        #     st.write("No motifs found.")
 
 app()
